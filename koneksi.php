67a9d646cf17253484a9815d971def9ec9e410a8
Xavier <gemparpanggih123@gmail.com>
2022-10-18 14:18:11 +0800

    update

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$user_name = "root";
 	$pass="";
-	$database_name ="ppob";
+	$database_name ="posttest)web";
 	$db_link = mysqli_connect($host,$user_name,$pass,$database_name);
 	if(!$db_link){
 		echo "Tidak Terhubung";
